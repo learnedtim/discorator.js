06ea71209cd1cdf84568381a18f3d8562071192e
Kai Beckers <kai.beckers.2008@gmail.com>
2024-10-06 10:14:05 +0200

    enshittify Gateway client, also fix some issues with it

--- discorator/src/client/Gateway.js
+++ discorator/src/client/Gateway.js
@@ -3,6 +3,13 @@ import Client from "./Client.js";
 import EventEmitter from "node:events";
 import Interaction from "../modules/server/ServerInteraction.js";
 
+// Current issues:
+// -> Reconnects are not working properly, well they work, they just disconnect after heartbeat > server requests heartbeat > client sends heartbeat > server acks > server disconnects
+//    why is this happening?
+// -> implement reIdentify resume mode
+// -> proper disconnecting
+// -> handle missed ACKs (must disconnect with any code besides 1000/1001)
+
 // whether to reconnect for each close code
 /**
  * @see https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-close-event-codes
@@ -54,6 +61,9 @@ export default class Gateway extends EventEmitter {
         this.connectReady = new Promise((resolve) => {
             this.connectResolve = resolve
         })
+        // externally abortable heartbeat
+        this.hbAbortController = null
+        this.exitHeartbeat = null
     }
 
     /**
@@ -81,8 +91,11 @@ export default class Gateway extends EventEmitter {
 
     /**
      * Reconnect to the gateway
+     * @param {Number} closeCode -- close code
+     * @param {Boolean} reIdentify -- whether to reidentify
      */
-    async resume(closeCode) {
+    async resume(closeCode, reIdentify = false) {
+        console.log('RESUME ISSUED WEEWOOWEEWOO')
         // If close code is unknown, reconnect. If one has been given, check if it allows for a reconnect
         if (closeCodes[closeCode]) {
             if (closeCodes[closeCode][0] == false) throw new Error("Reconnection failed: " + closeCodes[closeCode][1])
@@ -96,31 +109,38 @@ export default class Gateway extends EventEmitter {
         })
 
         // Reconnect
+        await this.exitHeartbeat()
         delete this.ws;
+        delete this.hb;
+        console.log(this.resumeGatewayUrl)
         this.ws = new WebSocket(this.resumeGatewayUrl);
         this.ws.on("open", this.onOpen.bind(this));
         this.ws.on("message", this.onMessage.bind(this)); 
         this.ws.on("close", this.onClose.bind(this));
         this.ws.on("error", this.onError.bind(this));
-
-        await this.hbReady; // wait for Hello to be sent
-
+        console.log('pre resume')
+        await this.hbReady; // wait for Hello to be sent and heartbeat to be established
+        console.log('resume sent')
         // send resume
         await this.send(6, {
             "token": this.#token,
             "session_id": this.sessionId,
             "seq": this.sequence
         })
-        
     }
 
     /**
      * Close the connection
      */
-    async close() {
+    async close(invalidateSession = false) {
+        console.log('FUNCTION CLOSE ISSUED WEEWOOWEEWOO')
         if (!this.ws) throw new Error("No connection to close");
         this.alive = false;
-        this.ws.close();
+        if (invalidateSession) {
+            this.ws.close(1000, "Session invalidated");
+        } else {
+            this.ws.close(1012, "Service Restart");
+        }
     }
 
     /**
@@ -130,6 +150,7 @@ export default class Gateway extends EventEmitter {
      */
     async send(op, data) {
         // temp
+        console.log('send:', { op, d: data})
         await this.ws.send(JSON.stringify({ op, d: data}))
     }
 
@@ -148,7 +169,9 @@ export default class Gateway extends EventEmitter {
     async onMessage(msg, isBinary) {
         msg = isBinary ? msg : msg.toString();
         msg = JSON.parse(msg);
-        //console.log(msg)
+        console.log('receive', msg)
+        if (msg.s !== null) this.sequence = msg.s;
+        console.log(this.sequence)
         switch (msg.op) {
             case 0:
                 // Event receive
@@ -158,7 +181,6 @@ export default class Gateway extends EventEmitter {
                     this.sessionId = msg.d.session_id
                     this.connectResolve()
                 }
-                this.sequence = msg.s;
                 //console.log(msg)
                 //this.dispatchEvent(msg.t, msg.d)
 
@@ -175,24 +197,28 @@ export default class Gateway extends EventEmitter {
                 break;
             case 1:
                 // Force Heartbeat
-                await this.send(1, null, this.sequence)
+                await this.send(1, this.sequence)
+                break;
+            case 7:
+                // Reconnect
+                await this.resume(4000)
+                break;
+            case 9:
+                // Invalid session
+                await this.resume(4009)
                 break;
             case 10:
                 // Hello
 
                 // if already connected, a heartbeat loop is not necessary
-                if (this.hbInterval) {
-                    this.hbResolve()
-                    return
-                }
+                //if (this.hbInterval) {
+                //    this.hbResolve()
+                //    return
+                //}
                 this.hbInterval = msg.d.heartbeat_interval;
                 this.alive = true;
-                this.heartbeat();
+                this.hb = this.heartbeat();
                 break;
-            case 11:
-                // Heartbeat ACK
-                this.sequence = msg.s;
-                break; 
             default:
                 //console.log(msg)
                 break;
@@ -216,6 +242,7 @@ export default class Gateway extends EventEmitter {
      */
     async onClose(msg, isBinary) {
         msg = isBinary ? msg : msg.toString();
+        console.log('close', msg)
 
         if (this.alive == false) { console.log("Socket connection closed."); return }
         if (this.alive == true)  console.log("Connection closed uncleanly with exit code: " + msg)
@@ -228,14 +255,23 @@ export default class Gateway extends EventEmitter {
      */
     async heartbeat() {
         if (!this.hbInterval) throw new Error("No heartbeat interval provided");
-        await new Promise(resolve => setTimeout(resolve, this.hbInterval * Math.random() * 0.1)); // NOTE: edited
-        await this.send(1, { s: this.sequence })
         await this.hbResolve()
-        do {
-            await new Promise(resolve => setTimeout(resolve, this.hbInterval));
-            await this.send(1, { s: this.sequence })
-        } while (this.alive);
+        await new Promise(resolve => setTimeout(resolve, this.hbInterval * Math.random())); // NOTE: edited
+        await this.send(1, this.sequence)
 
+        // this is a promise because it must be able to be externally aborted, and it is an infinite loop.
+        // todo: error handling, repeated non-handled errors may cause issues
+        this.heartbeatLoop = new Promise(async (resolve) => {
+            this.exitHeartbeat = resolve
+
+            do {
+                await new Promise(resolve => setTimeout(resolve, this.hbInterval));
+                await this.send(1, this.sequence)
+                console.log('loop heartbeat sent' + Date.now().toString())
+            } while (this.alive && this.exitHeartbeat == resolve);
+
+            resolve()
+        })
     }
 
     // Deprecated, use node:events instead
